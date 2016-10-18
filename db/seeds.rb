f501c624b3a28f372e3177a965eb14eb2ff924a4
B Kleinen <kleinen@infrastructure.de>
2016-10-18 08:03:00 +0200

    updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Habit.delete_all
-Habit.create(name: "Learn Vocabulary", frequency: 1, description: "Study English Vocabulary with Flash Cards",last: Date.today-1)
-Habit.create(name: "Buy Milk", frequency: 3, description: "Buy fresh milk",last: Date.today-4)
-Habit.create(name: "Read the Newspaper", frequency: 31, description: "Read at least a part",last: Date.today)
-Habit.create(name: "Clean Bathroom", frequency: 7, description: "Clean the bathroom",last: Date.today-7)
-Habit.create(name: "Buy Bread", frequency: 7, description: "Buy fresh Bread",last: Date.today-2)
+Habit.create(name: "Learn Vocabulary", frequency: 1, description: "Study English Vocabulary with Flash Cards",last_log_date: Date.today-1)
+Habit.create(name: "Buy Milk", frequency: 3, description: "Buy fresh milk",last_log_date: Date.today-4)
+Habit.create(name: "Read the Newspaper", frequency: 31, description: "Read at least a part",last_log_date: Date.today)
+Habit.create(name: "Clean Bathroom", frequency: 7, description: "Clean the bathroom",last_log_date: Date.today-7)
+Habit.create(name: "Buy Bread", frequency: 7, description: "Buy fresh Bread",last_log_date: Date.today-2)
