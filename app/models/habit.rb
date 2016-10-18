bc2d64a031971efe1662858014d49ffe192ce984
B Kleinen <kleinen@infrastructure.de>
2016-10-18 07:23:18 +0200

    Adapt Views/Controllers to new model

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,3 +1,9 @@
 class Habit < ApplicationRecord
   has_many :log_entries
+
+  def last_log_date
+    last_log = log_entries.order("date desc").first
+    last_log ? last_log.date : nil
+  end
+
 end
