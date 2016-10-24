57f421efa86d74904875896a45e37360d57da314
B Kleinen <kleinen@infrastructure.de>
2016-10-24 10:50:04 +0200

    week-3-5: added model test for last_log_date in habit

--- test/models/habit_test.rb
+++ test/models/habit_test.rb
@@ -3,9 +3,33 @@ require 'test_helper'
 class HabitTest < ActiveSupport::TestCase
   setup do
     @habit = habits(:walk)
+    @habit2 = habits(:litter)
   end
+
   test "make sure association works here" do
     assert_equal 1, @habit.log_entries.size
     assert_equal Date.new(2016,10,17), @habit.last_log_date
   end
+
+  test "habit can be logged" do
+    @habit2.last_log_date = Date.yesterday
+    assert_equal Date.yesterday, @habit2.last_log_date
+  end
+
+  test "habit keeps a history of log dates" do
+    @habit2.last_log_date = Date.yesterday
+    @habit2.last_log_date = Date.today
+    @habit2.last_log_date = Date.yesterday-1
+
+    assert_equal 3, @habit2.log_entries.size
+  end
+
+  test "last_log_date returns the most recent log" do
+    @habit2.last_log_date = Date.yesterday
+    @habit2.last_log_date = Date.today
+    @habit2.last_log_date = Date.yesterday-1
+
+    assert_equal Date.today, @habit2.last_log_date
+  end
+
 end
