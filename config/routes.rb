94bb4fd1e94d81a2e738c06d651d52f760915fe7
B Kleinen <kleinen@infrastructure.de>
2016-10-24 13:29:27 +0200

    set root path to habits index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :log_entries
-  resources :habits
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'habits#index'
   get '/habits/:id/log', to: 'habits#log', as: "log"
+
+  resources :habits
+  resources :log_entries
+  
 end
